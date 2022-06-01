5887562008158038b1169d71ebeb8bfe459e3b54
Abir-crypto <abdullahall.abir48@gmail.com>
2022-06-01 13:58:37 +0600

    node mongodb example part 2

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,5 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
+
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -11,28 +13,28 @@ MongoClient.connect(url, (err, client) => {
     console.log("Connected to the server");
 
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
+    
+    dboper.insertDocument(db, { "name": "dudu", "description": "yumm" }, 'dishes', (result) => {
+       
+        console.log('Insert document: \n', result);
 
-    collection.insertOne(
-        { "name": "Pizza", "description": "Akser moja" },
-        (err, result) => {
-            assert.equal(err, null);
+        dboper.findDocuments(db, 'dishes', (docs) => {
+            console.log('Found documents:\n', docs);
 
-            console.log("After insert:\n");
-            console.log(result);
+            dboper.updateDocument(db, { 'name': 'dudu' }, { 'description': 'so soft and tasty' }, 'dishes', (result) => {
+                
+                console.log('Updated Document: \n', result);
+                
+                dboper.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found Documents: \n', docs);;
+                    db.dropCollection('dishes', (docs) => {
+                        console.log('Dropped Collection: ', docs);
 
-            collection.find({}).toArray((err, docs) => {
-                assert.equal(err, null);
+                        client.close();
+                    });
+                });
+            });
+        });
+    });
 
-                console.log("Found: \n");
-                console.log(docs);
-
-                db.dropCollection('dishes', (err, result) => {
-                    assert.equal(err, null);
-
-                    client.close()
-                })
-            })
-        })
-
-})
+});
